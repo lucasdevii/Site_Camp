b240ce824458f421c026b9239c7cb2b4286c9a24
Lucas Sousa <lucassousa180810@gmail.com>
2025-08-09 23:56:38 -0300

    Criação dos tópicos site-bar do usuário

--- meu-app/src/components/sideBarInfo.tsx
+++ meu-app/src/components/sideBarInfo.tsx
@@ -15,23 +15,31 @@ function SideBar({ setOpenSideBar }: SideBarProps) {
 
   return (
     <div className="rounded-l-2xl px-2">
-      <div className="flex items-center justify-between">
-        <div>
-          <div className="w-8">
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex h-8">
+          <div className="w-8 mr-1">
             <img
               src="circle-person-profile-user-group-people-svgrepo-com.svg"
               alt=""
             />
           </div>
           <div className="">
-            <p>{`${nameUser}`}</p>
-            <p>{`${isLogged}`}</p>
+            <p className="text-xs">{`${nameUser}`}</p>
+            <p className="text-xs">{`${isLogged}`}</p>
           </div>
         </div>
         <div className="" onClick={() => setOpenSideBar(false)}>
           <p className="text-sm text-red-600 cursor-pointer">X</p>
         </div>
       </div>
+      <div className="h-8 w-full mb-1 bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+      <div className="h-8 mb-1 w-full bg-stone-800 rounded-sm border border-stone-700"></div>
+
       <div></div>
     </div>
   );
